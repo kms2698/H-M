8f408bb09decb435d6894969a7948b640be350fd
kms2689 <kms2698@gmail.com>
2021-05-12 21:40:43 +0900

    update 05.12

--- test.php
+++ test.php
@@ -54,15 +54,13 @@
                 /* --------------현재위치 함수 --------------*/
                 // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
                 if (navigator.geolocation) {
-                    
                     // GeoLocation을 이용해서 접속 위치를 얻어옵니다
                     navigator.geolocation.getCurrentPosition(function(position) {
                         
                         var lat = position.coords.latitude, // 위도
                             lon = position.coords.longitude; // 경도
                         
-                        var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-                            message = '<div style="padding:5px;text-align:center">현재 나의 위치</div>'; // 인포윈도우에 표시될 내용입니다
+                        var locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
                         
                         // 마커와 인포윈도우를 표시합니다
                         displayMarker(locPosition, message);
@@ -84,8 +82,7 @@
                             
                     });
                     
-                } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-                    
+                } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다 -> 학교 위치로 설정할것임
                     var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),    
                         message = 'geolocation을 사용할수 없어요..'
                         
@@ -106,19 +103,6 @@
                         clickable: true, // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
                         image : markerImage // 마커 이미지 
                     }); 
-                    
-                    var iwContent = message, // 인포윈도우에 표시할 내용
-                        iwRemoveable = false;
-
-                    // 인포윈도우를 생성합니다
-                    var infowindow = new kakao.maps.InfoWindow({
-                        content : iwContent,
-                        removable : iwRemoveable
-                    });
-                    
-                    // 인포윈도우를 마커위에 표시합니다 
-                    infowindow.open(map, marker);
-                    
                     // 지도 중심좌표를 접속위치로 변경합니다
                     map.setCenter(locPosition);    
 
@@ -218,6 +202,12 @@
                         content: "상록구청주차장",
                         latlng: new kakao.maps.LatLng(37.300250220584346, 126.84587834962909),
                     },
+
+                    // 집에서 테스트용
+                    // {   
+                    //     content: "테스트",
+                    //     latlng: new kakao.maps.LatLng(37.44942392756917, 126.68314181506508),
+                    // },
 
                 ];
                 // 마커 이미지 추가 /Red/Green 상황에 따라 변화 예정
